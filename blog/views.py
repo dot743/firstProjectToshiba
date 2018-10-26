48a6b740b82417ffc0b6c74705c8a979ed4b6bf2
Nguyen <tan.nguyen@orangeusd.org>
2018-10-26 16:24:32 -0700

    added loop

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,8 @@ from .models import *
 def home(request, flipper):
 	if request.method == 'GET':
 		post = Post.objects.get(id=flipper)
-		return render(request, 'blog/home.html', {'post':post})
+		post_list = [post, post, post, post, post]
+		return render(request, 'blog/home.html', {'aerhgui':post_list})
 
 def home2(request):
 	return render(request, 'blog/home2.html')
